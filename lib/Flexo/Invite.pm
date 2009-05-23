0ca2f8c2ab8fe7648a39314d417e2ffc4f881463
Chris Prather <chris@prather.org>
2009-05-23 03:41:33 -0400

    enable more namespace::autoclean

--- lib/Flexo/Invite.pm
+++ lib/Flexo/Invite.pm
@@ -1,11 +1,11 @@
 package Flexo::Invite;
 use Moses::Plugin;
+use namespace::autoclean;
 
 events qw(invite bot_addressed);
 
 sub S_invite {
     my ( $self, $irc, $nickstr, $channel ) = @_;
-
     $irc->yield( join => $$channel );
     return PCI_EAT_PLUGIN;
 }
@@ -13,13 +13,11 @@ sub S_invite {
 sub S_bot_addressed {
     my ( $self, $irc, $nickstr, $channel, $msg ) = @_;
     if ( $$msg =~ /^join (\#.*)/ ) {
-
         $irc->yield( join => $1 );
         return PCI_EAT_PLUGIN;
     }
     return PCI_EAT_NONE;
 }
 
-no Moses::Plugin;
 1;
 __END__
