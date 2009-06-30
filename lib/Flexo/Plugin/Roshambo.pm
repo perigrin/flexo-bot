4794383cb6ec08774ba9c90d7ec736e7504e508b
Chris Prather <chris@prather.org>
2009-06-30 01:07:11 -0400

    refactor so the parser is cleaner

--- lib/Flexo/Plugin/Roshambo.pm
+++ lib/Flexo/Plugin/Roshambo.pm
@@ -1,6 +1,7 @@
 package Flexo::Plugin::Roshambo;
 use Moses::Plugin;
 use Games::Roshambo;
+use List::Util qw(shuffle);
 
 use namespace::autoclean;
 
@@ -17,15 +18,14 @@ sub _build_lines {
     [
         'I had _CHOICE_, _WINNER_ wins.',
         '_WINNER_ wins! (I had _CHOICE_.)',
-        'I chose _CHOICE_ ... the winner is _WINNER_.',
+        'I choose _CHOICE_ ... the winner is _WINNER_.',
     ];
 }
 
 sub line {
-    my ( $self, $win, $choice ) = @_;
-    my @LINES = $self->lines;
-    my $line  = $LINES[ rand $#LINES ];
-    $line =~ s/_WINNER_/$win/g;
+    my ( $self, $winner, $choice ) = @_;
+    my ($line) = shuffle $self->lines;
+    $line =~ s/_WINNER_/$winner/g;
     $line =~ s/_CHOICE_/$choice/g;
     return $line;
 }
@@ -37,28 +37,24 @@ has rps => (
     handles    => [qw(judge)],
 );
 
-sub _build_rps {
-    Games::Roshambo->new( numthrows => 101 );
-}
+sub _build_rps { Games::Roshambo->new( numthrows => 101 ) }
 
 sub play {
     my ( $self, $you, $from ) = @_;
-    my $me = $self->rps->num_to_name( $self->rps->gen_throw);
-    return $self->line( 'nobody',    $me ) if $self->judge( $you, $me ) == 0;
-    return $self->line( $from,       $me ) if $self->judge( $you, $me ) == 1;
-    return $self->line( $self->nick, $me ) if $self->judge( $you, $me ) == 2;
+    my $me = $self->rps->gen_throw;
+    my $winner = ( 'nobody', $from, $self->nick )[ $self->judge( $you, $me ) ];
+    return $self->line( $winner, $self->rps->num_to_name($me), );
 }
 
 sub S_msg {
     my ( $self, $irc, $nickstring, $channels, $message ) = @_;
+    my $choice = $self->rps->name_to_num($$message);
+    return PCI_EAT_NONE unless $choice;
+
     my $channel = $$channels->[0] || '';
-    if ( $$message =~ qr/^\s*(rock|paper|scissors)\s*$/i ) {
-        my $choice = lc($1);
-        my $nick   = parse_user($$nickstring);
-        $self->privmsg( $channel => $self->play( $choice, $nick ) );
-        return PCI_EAT_PLUGIN;
-    }
-    return PCI_EAT_NONE;
+    my $result = $self->play( $choice, parse_user($$nickstring) );
+    $self->privmsg( $channel => $result );
+    return PCI_EAT_PLUGIN;
 }
 
 __PACKAGE__->meta->add_method( S_bot_addressed => \&S_msg );
