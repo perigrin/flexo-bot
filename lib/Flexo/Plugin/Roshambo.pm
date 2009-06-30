f7566584563ab917f79d6b5bb4b9a2406f2d5610
Chris Prather <chris@prather.org>
2009-06-30 00:36:50 -0400

    get Roshambo ported over to Moses::Plugin and Games::Roshambo

--- lib/Flexo/Plugin/Roshambo.pm
+++ lib/Flexo/Plugin/Roshambo.pm
@@ -1,110 +1,67 @@
-package Bender::Games::Roshambo;
-use strict;
-use warnings;
-use Carp;
+package Flexo::Plugin::Roshambo;
+use Moses::Plugin;
+use Games::Roshambo;
 
-use version; qv(0.03);
-use POE::Component::IRC::Plugin qw(:ALL);
-use POE::Component::IRC::Common qw( :ALL );
-use Regexp::Common qw(IRC);
+use namespace::autoclean;
 
-use Moose;
-with qw(Bender::Role::Storage::YAML);
-
-has depends => ( 
-	isa => 'ArrayRef', 
-	is => 'ro', 
-	auto_deref => 1,
-	default => sub { [qw(Bender::Core::BotAddressed)] },
-);
+events qw(bot_addressed msg);
 
 has lines => (
-	isa => 'ArrayRef',
-	is => 'ro',
-	auto_deref => 1,
-	lazy => 1,
-	default => sub { [ map { chomp; $_ } <DATA> ] },
+    isa        => 'ArrayRef',
+    is         => 'ro',
+    auto_deref => 1,
+    lazy_build => 1,
 );
 
-sub BUILD {
-	my ($self) = @_;
-	# alias S_public to S_msg because they do the same thing
-	$self->meta->alias_method(S_public => \&S_msg);
-	# alias S_irc_bot_addressed to S_msg because they do the same thing
-	$self->meta->alias_method(S_bot_addressed => \&S_msg);
+sub _build_lines {
+    [
+        'I had _CHOICE_, _WINNER_ wins.',
+        '_WINNER_ wins! (I had _CHOICE_.)',
+        'I chose _CHOICE_ ... the winner is _WINNER_.',
+    ];
 }
 
-sub read { 
-	my ($self, $data) = @_;
-	$self->{data} = $data;
-}
-
-sub data { 
-	my ($self) = @_;
-	return $self->{data};
-}
-
-my @MOVES = qw( rock paper scissors );
-
-sub PCI_register {
-    my ( $self, $irc ) = @_;
-    $self->{irc} = $irc;
-    $irc->plugin_register( $self, 'SERVER', qw(bot_addressed public msg) );
-    return 1;
-}
-
-sub PCI_unregister {
-    my ( $self, $irc ) = @_;
-    delete $self->{irc};
-    return 1;
-}
-
-sub choose {
-    my ( $self, $what, $where, $from ) = @_;
-    my $bot    = $self->{irc};
-    my $choice = lc $MOVES[ rand @MOVES ];
-    my $me     = $bot->nick_name;
-    my $win =
-        $choice eq $what ? 'nobody'
-      : $choice eq 'rock'     && $what eq 'paper'    ? $from
-      : $choice eq 'rock'     && $what eq 'scissors' ? $me
-      : $choice eq 'paper'    && $what eq 'rock'     ? $me
-      : $choice eq 'paper'    && $what eq 'scissors' ? $from
-      : $choice eq 'scissors' && $what eq 'rock'     ? $from
-      : $choice eq 'scissors' && $what eq 'paper'    ? $me
-      :                                                'nobody';
+sub line {
+    my ( $self, $win, $choice ) = @_;
     my @LINES = $self->lines;
-    my ( $action, $line ) = split( /\s+/, $LINES[ rand $#LINES ], 2 );
-    $line   =~ s/_WINNER_/$win/g;
-    $line   =~ s/_CHOICE_/$choice/g;
-    $action =~ s/_/ /g;
-    $self->record_winner($win);
-    $bot->yield( lc($action) => $where => $line );
-    return 1;
+    my $line  = $LINES[ rand $#LINES ];
+    $line =~ s/_WINNER_/$win/g;
+    $line =~ s/_CHOICE_/$choice/g;
+    return $line;
 }
 
-sub record_winner {
-    my ( $self, $winner ) = @_;
-    $self->{data}{winners}{$winner}++;
-    $self->save();
+has rps => (
+    isa        => 'Games::Roshambo',
+    is         => 'ro',
+    lazy_build => 1,
+    handles    => [qw(judge)],
+);
+
+sub _build_rps {
+    Games::Roshambo->new( numthrows => 101 );
+}
+
+sub play {
+    my ( $self, $you, $from ) = @_;
+    my $me = $self->rps->num_to_name( $self->rps->gen_throw);
+    return $self->line( 'nobody',    $me ) if $self->judge( $you, $me ) == 0;
+    return $self->line( $from,       $me ) if $self->judge( $you, $me ) == 1;
+    return $self->line( $self->nick, $me ) if $self->judge( $you, $me ) == 2;
 }
 
 sub S_msg {
     my ( $self, $irc, $nickstring, $channels, $message ) = @_;
-    ( $message, $channels ) = ( $$message, $$channels );
-    $irc->{plugin_debug} = 1;
-    my $channel = $channels->[0] || '';
-    my $from    = parse_user($$nickstring);
-    my $CMD     = qr/^\s*(rock|paper|scissors)\s*$/i;
-    if ( $message =~ $CMD ) {
-        $self->choose( lc($1), $channel, $from );
+    my $channel = $$channels->[0] || '';
+    if ( $$message =~ qr/^\s*(rock|paper|scissors)\s*$/i ) {
+        my $choice = lc($1);
+        my $nick   = parse_user($$nickstring);
+        $self->privmsg( $channel => $self->play( $choice, $nick ) );
         return PCI_EAT_PLUGIN;
     }
     return PCI_EAT_NONE;
 }
 
+__PACKAGE__->meta->add_method( S_bot_addressed => \&S_msg );
+
 1;
-__DATA__
-PRIVMSG I had _CHOICE_, _WINNER_ wins.
-PRIVMSG _WINNER_ wins! (I had _CHOICE_.)
-PRIVMSG I chose _CHOICE_ ... the winner is _WINNER_.
+__END__
